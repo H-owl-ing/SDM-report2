468375b47f174c5e4bf28b9f2f1cf36d7fa18095
高橋　龍太朗 <chovivi@chobi.local>
2025-02-11 21:44:35 +0900

    hogehoge

--- test_cases.py
+++ test_cases.py
@@ -19,3 +19,14 @@ class TestCalc (unittest.TestCase):
         def test_sample4 (self):
                 self.assertEqual (-1, calc(0.1,999))
 
+
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(1,998.1))
+
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(1.1,999))
+
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(1,998.1))
+
+
